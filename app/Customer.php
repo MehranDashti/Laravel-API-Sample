e88673cb210563d4001fe7a2979aae299435e560
Vahid Rastgoo <v7hidrastgoo@gmail.com>
2020-10-25 16:29:21 +0330

    Add "hidden" field & "getFullNameAttribute" method.

--- app/Customer.php
+++ app/Customer.php
@@ -14,4 +14,23 @@ class Customer extends Model
     protected $fillable = [
         'first_name', 'last_name', 'phone',
     ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id', 'created_at', 'updated_at'
+    ];
+
+    /**
+     * Get the user's full name.
+     *
+     * @return string
+     */
+    public function getFullNameAttribute()
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }
